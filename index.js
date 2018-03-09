95e2de9b134690534046004988f6dcfea0dec1f2
Poobest Jirathitiphuwadon <>
2018-03-09 15:21:03 +0700

    http method post/get

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res){
+
+});
+
+app.post('/', function(req, res){
+
+});
 
 var server = app.listen(3000, function(){
     console.log("Start API AIRNODE Successful !!");
